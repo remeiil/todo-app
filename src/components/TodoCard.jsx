3ea31de0fcd049f654040b4ee9acd2a5a466c931
remeiil <153274273+remeiil@users.noreply.github.com>
2025-03-27 22:57:55 +1300

    cleaned up edit function and added abbrs

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 export function TodoCard(props) {
     const { todo, handleDeleteTodo, todoIndex, handleCompleteTodo, handleEditTodo } = props
     const [editedInputValue, setEditedInputValue] = useState('')
+    const [isEditing, setIsEditing] = useState(false)
     
     return (
         <div className="taskCard">
@@ -10,32 +11,43 @@ export function TodoCard(props) {
                 <p>{todo.input}</p>
             </div>    
             <div className="taskMain">
-                <p>Edit: </p>
-                <input value={editedInputValue} onChange={(e) => {
-                    setEditedInputValue(e.target.value)
-                }} placeholder={todo.input} />
-                <button onClick={() => {
-                    handleEditTodo(todoIndex, editedInputValue)
-                }} disabled={todo.complete}>
-                    <i className="fa-regular fa-pen-to-square"></i>
-                </button>
+                {
+                    (!isEditing  ? 
+                        <button onClick={() => {
+                            setIsEditing(true)
+                        }}>
+                            <abbr title="Edit task name"><i className="fa-regular fa-pen-to-square"></i></abbr>
+                        </button> :
+                        <>
+                            <input value={editedInputValue} onChange={(e) => {
+                                setEditedInputValue(e.target.value)
+                            }} placeholder={todo.input} />
+                            <button onClick={() => {
+                                handleEditTodo(todoIndex, editedInputValue)
+                                setIsEditing(false)
+                            }} disabled={todo.complete}>
+                                <abbr title="Save task name"><i className="fa-regular fa-floppy-disk"></i></abbr>
+                            </button>
+                        </>
+                    )
+                }
             </div>
             <div className="taskExpandable">
                 <p>Estimated Duration:</p><p> {todo.taskDuration}</p>
             </div>
             <div className="taskFooter">
                 <div class="categories">
-                    <p>{todo.taskCategory}</p>
+                    <abbr title={"Category: " + todo.taskCategory}><p>{todo.taskCategory}</p></abbr>
                 </div>
                 <button onClick={() => {
                     handleCompleteTodo(todoIndex)
                 }} disabled={todo.complete}>
-                    <i className="fa-regular fa-square-check"></i>
+                    <abbr title="Mark Completed"><i className="fa-regular fa-square-check"></i></abbr>
                 </button>
                 <button onClick={() => {
                     handleDeleteTodo(todoIndex)
                 }}>
-                    <i className="fa-regular fa-trash-can"></i>
+                    <abbr title="Delete Entirely"><i className="fa-regular fa-trash-can"></i></abbr>
                 </button>
             </div>
         </div>
